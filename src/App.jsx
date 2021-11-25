95f998077834f39bb223a6a556745063b369a650
amarpsp10 <amarpsp10@gmail.com>
2021-11-25 10:48:49 -0800

    Css module and Assets loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import './styles/index.css'
+import classes from './styles/index.css'
+import Logo from './assets/react.png'
 
 const App = () => {
     return (
         <>
-            <section className={'hero'}>
+            <section className={classes.hero}>
                 <h3>Hi, from App.jsx Component</h3>
+            </section>
+            <section>
+                <h3>here is another section of images loaded directly in image tag</h3>
+                <img height="200px" src={Logo} alt='logo' />
             </section>
         </>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,23 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = "development";
+let target = "web";
 if (process.env.NODE_ENV === "production") {
     mode = "production";
 }
 
 module.exports = {
     mode: mode,
-
+    target: target,
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource"
+            },
+            {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, { loader: "css-loader", options: { modules: true } }],
             },
             {
                 test: /\.jsx?$/,
